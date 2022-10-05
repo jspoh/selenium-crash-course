c24586a2cadf7008280cd0cbc01a11b523c2758e
js <99598950+jspoh@users.noreply.github.com>
2022-10-05 19:42:41 +0800

    Update sel-crashcourse.py

--- sel-crashcourse.py
+++ sel-crashcourse.py
@@ -12,11 +12,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 options.add_experimental_option('detach', True)
 
-PATH = 'D:\Programming\chromedriver_win32/chromedriver.exe'
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get('https://google.com')
-assert 'Google' in driver.title
-googleSearch = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-googleSearch.send_keys('hihi wait no let me delete this')
-googleSearch.clear()
-googleSearch.send_keys('test', Keys.ENTER)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))  # initialize a driver
+driver.get('https://google.com')  # navigate to page
+assert 'Google' in driver.title  # ensure that page is correct
+googleSearch = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')  # get element by xpath (there are extensions to help)
+googleSearch.send_keys('hihi wait no let me delete this')  # send keystrokes
+googleSearch.clear()  # clear input (if its in an input field)
+googleSearch.send_keys('test', Keys.ENTER)  # send keystrokes and press enter

a9f3fb515e0b89c8c06aa389feabcb31e921b63c
js <99598950+jspoh@users.noreply.github.com>
2022-10-06 00:32:39 +0800

    Update sel-crashcourse.py

--- sel-crashcourse.py
+++ sel-crashcourse.py
@@ -12,10 +12,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 options.add_experimental_option('detach', True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))  # initialize a driver
+# MAKE IT SO THAT THE SELENIUM BROWSER DOES NOT POP OUT
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)  # initialize a driver with options selected on top
+
 driver.get('https://google.com')  # navigate to page
+
 assert 'Google' in driver.title  # ensure that page is correct
-googleSearch = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')  # get element by xpath (there are extensions to help)
+
+googleSearch = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')  # get element by absolute xpath (there are extensions to help)
+googleSearch = driver.find_element(By.XPATH, "//input[@aria-label='Search']")  # another way to select the element
+
 googleSearch.send_keys('hihi wait no let me delete this')  # send keystrokes
 googleSearch.clear()  # clear input (if its in an input field)
 googleSearch.send_keys('test', Keys.ENTER)  # send keystrokes and press enter
